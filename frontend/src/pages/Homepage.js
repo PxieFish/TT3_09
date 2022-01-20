c8bbf306466979b9ce0f5291533704de6c9bb7f9
YippyYaYa <yippyyeehaw@hotmail.com>
2022-01-20 14:36:58 +0800

    added like counter

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -2,25 +2,25 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import IndivPost from '../components/IndivPost';
 
+function Like() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p> {count} likes </p>
+            <Button onClick={() => setCount(count + 1)}>Like</Button>
+        </div>
+    );
+}
+
 export default function Homepage() {
     return (
         <div className='homepage'>
             <h1>Homepage</h1>
             <div>
                 <IndivPost />
-                {/*{Like}*/}
+                <Like />
             </div>
         </div>
     );
 }
-
-//function Like() {
-//    const [count, setCount] = useState(0);
-
-//    return (
-//        <div>
-//            <p> {count} likes </p>
-//            <Button onClick={() => setCount(count + 1)}>Like</Button>
-//        </div>
-//    );
-//}
